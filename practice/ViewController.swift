0066c29815374e83acb6c57c0940e23829fe02c7
XinYan <103906642+XinYanC@users.noreply.github.com>
2022-06-24 09:17:42 -0400

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by XinYan Chen on 6/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
